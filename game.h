fde74e33db95cd470b6f17ba300c00755c47a427
Nikita Sergievsky <ReetRay@protonmail.com>
2021-06-01 16:00:34 +0300

    Рефакторинг класса Card. Новый класс Deck

--- game.h
+++ game.h
@@ -1,39 +1,46 @@
 #include <vector>
 #include <string>
+#include <random>
+#include <iostream>
+#include <algorithm>
+#include <ctime>
 
 const std::string cardsuits[4] = {"Черви","Вини","Буби","Крести"};
 const std::string cardvalues[13] = {"2","3","4","5","6","7","8","9","10","Валет","Дама","Король","Туз"};
 
 class Card{
-    private:
-        short cardsuit;
-        short cardvalue;
     public:
-        short suit(){
-            return this->cardsuit;
-        };
-
-        short value(){
-            return this->cardvalue;
-        };
-        std::string cmd_view(){
-            return cardvalues[this->cardvalue] + " " + cardsuits[this->cardsuit];
-        }
-    Card() = default;
-    Card(short new_suit, short new_value){
-            this->cardsuit = new_suit;
-            this->cardvalue = new_value;    
-        };
+    int suit;
+    int value;
 };
 
-std::vector<Card> generate_deck(){
-    std::vector<Card> new_deck = {};
-    for (short gen_value = 0; gen_value < 13; gen_value++){
-        for (short gen_suit = 0; gen_suit < 4; gen_suit++){
-            new_deck.push_back(Card(gen_suit, gen_value));
-        } 
-    }
-    return new_deck;
+class Deck{
+    private:
+        Card cards[52];
+        int top;
+    public:
+        void shuffle(){
+            this->top = 51; // Защита от неперемешаннной колоды
+            srand(time(0)); // Фикс бага при компиляции задается единый сид рандома
+            std::random_shuffle(std::begin(this->cards), std::end(this->cards));
+        }
+        void cmd_print(){
+            for (int i=0;i<52;i++)
+                std::cout << cardvalues[cards[i].value] << cardsuits[cards[i].suit] << std::endl;
+            std::cout << std::endl;
+        }
+        Card take_card(){
+            this->top--;
+            return this->cards[this->top+1];
+        }
+        Deck(){
+            for (int i=0;i<4;i++){
+                for (int j=0;j<13;j++){
+                    cards[i*13+j].suit = i;
+                    cards[i*13+j].value = j;
+                }
+            }
+        }
 };
 
 class Player{
--- main.cpp
+++ main.cpp
@@ -3,13 +3,23 @@
 #include <vector>
 #include "game.h"
 
+
 int main(){
     Player p1(193802139012);
     p1.nickname = "yepIwt";
-    std::vector<Card> deck = generate_deck();
+    Deck deck;
+    deck.shuffle();
+    deck.cmd_print();
+    Card a = deck.take_card();
+    Card b = deck.take_card();
+    Card c = deck.take_card();
+    std::cout << cardvalues[a.value] << cardsuits[a.suit] << std::endl; 
+    std::cout << cardvalues[b.value] << cardsuits[b.suit] << std::endl;
+    std::cout << cardvalues[c.value] << cardsuits[c.suit] << std::endl;
     // for (Card c : deck){
     //     std::cout << "Card Info: " << c.cmd_view() << std::endl; 
     // }
+    
     std::cout << "Player info: " << p1.nickname << std::endl;
     std::cout << "Player stack: " << p1.get_in_game_stack() << std::endl;  
     return 0;
