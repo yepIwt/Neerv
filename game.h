c445131b1e82891c586de81867aa268fbe6594cb
Nikita Sergievsky <ReetRay@protonmail.com>
2021-06-01 16:07:50 +0300

    Рефакторинг класса Player

--- game.h
+++ game.h
@@ -44,26 +44,11 @@ class Deck{
 };
 
 class Player{
-    private:
-        std::pair<Card,Card> hand_cards = {};
-        unsigned long long in_game_stack = 0;
     public:
-        std::string nickname = "";
-        bool have_cards = false;
-        void take_hand_cards(std::pair<Card,Card> new_hand_card){
-            // Проверка на то, что уже есть карты
-            this->hand_cards = new_hand_card;
-        }
-        std::pair<Card,Card> get_hand_cards(){
-            return this->hand_cards;
-        }
-        unsigned long long get_in_game_stack(){
-            return this->in_game_stack;
-        }
-        Player() = default;
-        Player(unsigned long long new_in_game_stack){
-            this->in_game_stack = new_in_game_stack;
-        }
-        
-
+        std::string nickname;
+        unsigned long int money;
+        Card hand_cards[2];
+        int is_game_rn;
+        int move;
+        int thinks;
 };
